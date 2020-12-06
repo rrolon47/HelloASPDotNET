de4111fdb1ec646690b6f706d6566bd84ad182f3
Rachel <rachel.rolon@gmail.com>
2020-12-05 18:26:53 -0600

    updated language selection feature

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -9,27 +9,55 @@ namespace HelloASPDotNET.Controllers
     [Route("/helloworld/")]
     public class HelloController : Controller
     {
-        //GET: /<controllers>/
+        public static string CreateMessage(string name, string language)
+        {
+            string html = "";
+
+            if (String.IsNullOrEmpty(language) && String.IsNullOrEmpty(name))
+            {
+                html = $"<h1>Welcome to my app, World!</h1>";
+            }
+            else if (language.Equals("en"))
+            {
+                html = $"<h1>Welcome to my app, {name}!</h1>";
+            }
+            else if (language.Equals("fr"))
+            {
+               html = $"<h1>Bonjour, {name}!</h1>";
+            }
+            else if (language.Equals("sp"))
+            {
+                html = $"<h1>&#161Buenvinidos a mi app, {name}!</h1>";
+
+            }
+            return html;
+        }
+        
+       //GET: /<helloworld>/
        [HttpGet]
-       
         public IActionResult Index()
         {
             string html = "<form method='post' action='/helloworld/welcome'>" +
                 "<input type = 'text' name = 'name' />" +
+                "<select name = 'language'>"+
+                    "<option value = ''> --Select a Language-- </option>" +
+                    "<option value = 'en'> English </option>" +
+                    "<option value = 'fr'> French </option>" +
+                    "<option value ='sp'> Spanish </option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet me!' />" +
                 "</form>";
             return Content(html, "text/html");
         }
         
         //GET: helloworld/welcome/name/
-        //POST: hlloworld/welcome/
+        //POST: helloworld/welcome/
         [HttpGet("welcome/{name?}")]
         [HttpPost("welcome")]
-         public IActionResult Welcome(string name = "World")
+         public IActionResult Welcome(string name = "World" , string language = "en")
          {
-             string html = $"<h1>Welcome to my app, {name}!</h1>";
+            string html = CreateMessage(name, language);
              return Content(html, "text/html");
-
          }
 
     //Exercise
@@ -40,8 +68,10 @@ namespace HelloASPDotNET.Controllers
             string html = "<form method='post' action='/helloworld/language_greeting'>" +
                 "<input type = 'text' name = 'name' />" +
                 "<select name = 'language'>" +
-                "   <option>English</option>" +
-                "   <option>Spanish</option>" +
+                "   <option value = ''> --Select a Language-- </option>" +
+                "   <option value ='en'>English</option>" +
+                "   <option value ='sp'>Spanish</option>" +
+                "   <option value = 'fr'>French</option>" +
                 "</select>" +
                 "<input type = 'submit' value = 'Greet me!' />" +
                 "</form>";
@@ -52,22 +82,7 @@ namespace HelloASPDotNET.Controllers
        [HttpPost("language_greeting")]
         public IActionResult WelcomeInLanguage(string name, string language)
         {
-            
-            if (language == "English")
-            {
-                return Content($"<h1>Welcome to my app, {name}!</h1>", "text/html");
-            }
-            if (language == "Spanish")
-            {
-                return Content($"<h1>&#161Buenvinidos a mi App, {name}!</h1>", "text/html");
- 
-            }
-            else
-            {
-                return Content($"<h1>Welcome to my app, World!</h1>", "text/html");
-            }
-            
-
+            return Content(CreateMessage(name, language), "text/html");
         }
 
     }
